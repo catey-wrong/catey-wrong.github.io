c0a30b6ce743c080e5b8775fdf89ddcf6c2378f7
catey-wrong <Catey.m.wright@icloud.com>
2025-04-10 12:40:34 -0400

    huh

---
+++ functions.js
@@ -0,0 +1,23 @@
+function hi(name){
+   console.log("hii " + name);
+}
+
+hi("Catey");
+
+function f(x) {
+	console.log(x+1)
+}
+
+f(3);
+f(4);
+f(5);
+
+function circle() {
+ctx.fillstyle="red"
+ctx.beginPath();
+ctx.arc(x,y,25,0,2*math.PI);
+ctx.stroke();
+ctx.fill();
+}
+circle(100,100);
+
